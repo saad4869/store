9b02fac2505d0048fecd59fcb1df771982eeb318
HADDADI <saad.haddadi-ext@yrnet.com>
2025-03-22 16:09:51 +0000

    Database : add product entity and db properties

---
+++ src/main/java/org/imedia/store/db/entity/Product.java
@@ -0,0 +1,33 @@
+package org.imedia.store.db.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @Column(name = "sku", nullable = false)
+    private String sku;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price", nullable = false)
+    private BigDecimal price;
+
+}
